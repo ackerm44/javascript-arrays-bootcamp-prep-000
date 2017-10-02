3d9750c682f24337e4632eab59fe84fd38caa3e5
Amanda Ackerman <ackerm44@gmail.com>
2017-10-02 20:08:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,7 +21,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return days.shift();
+  return array.shift();
 }
 
 function removeElementFromBeginningOfArray(array) {
